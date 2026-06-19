488f57b824818b690bb114d08100c0ffaea36f37
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-06-19 10:58:11 +0530

    Pointer pointing forwards

--- pushzeroesatend.c
+++ pushzeroesatend.c
@@ -5,12 +5,15 @@ void swap(int *a,int *b){
     *b=temp;
 }
 void main(){
-    int arr[]={1,2,3,4,5,4,0,6,0};
+    int arr[]={1, 2, 0, 4, 3, 0, 5, 0};
     for(int i=0;i<sizeof(arr)/sizeof(int);i++){
-        printf("%d ",arr[i]);
-        if(arr[i]==0 && arr[i+1]!=0){
+       // printf("%d ",arr[i]);
+        if(arr[i]==0 && (arr[i+1]>0 || arr[i+1]<0)){
             
             swap(&arr[i+1],&arr[i]);
+            if(arr[i]==0 && arr[i+1]==0){
+                arr[i+1]=arr[i];
+            }
         }
     }
     printf("\nSize of array initially: %d \n",sizeof(arr)/sizeof(int));
