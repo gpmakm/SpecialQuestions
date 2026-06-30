ca80cd41d60b5230c9333cdac5795efa9ddd9c2d
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-06-30 22:08:35 +0530

    Elemtns are pushing towards last but arrangement is different

--- pushnegsatend.c
+++ pushnegsatend.c
@@ -6,18 +6,34 @@ void swap(int *a,int *b){
 }
 int main(){
     int arr[]={1,2,3,4,-5,4,-9,6,-10};
-    for(int i=0;i<sizeof(arr)/sizeof(int);i++){
-        printf("%d ",arr[i]);
-        if((arr[i]<0 && arr[i+1]>=0)){
+    // for(int i=0;i<sizeof(arr)/sizeof(int);i++){
+    //     printf("%d ",arr[i]);
+    //     if((arr[i]<0 && arr[i+1]>=0)){
             
-            swap(&arr[i+1],&arr[i]);
-        }
-    }
-    printf("\nSize of array initially: %d \n",sizeof(arr)/sizeof(int));
+    //         swap(&arr[i+1],&arr[i]);
+    //     }
+    // }
+    // printf("\nSize of array initially: %d \n",sizeof(arr)/sizeof(int));
     for(int i=0;i<sizeof(arr)/sizeof(int);i++){
         printf("%d ",arr[i]);
     }
-    printf("\nSize of array after: %d ",sizeof(arr)/sizeof(int));
+    //printf("\nSize of array after: %d ",sizeof(arr)/sizeof(int));
+    printf("\n");
+    int i=0,j=sizeof(arr)/sizeof(int)-1;
+    while(i<j){
+        if(arr[i]<0 && !(arr[j]<0)){
+            swap(&arr[i],&arr[j]);
+           
+            j--;
+        }
+        i++;
+         if(arr[j]<0){
+                j--;
+            }
+    }
+    for(int i=0;i<sizeof(arr)/sizeof(int);i++){
+        printf("%d ",arr[i]);
+    }
    
      return 0;
 }
