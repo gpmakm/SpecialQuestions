1907c8f41d8f1cfc45cfd073e65115841daaaf75
gpmakm <esikshatutorialsakarsh@gmail.com>
2026-07-07 00:12:01 +0530

    Added a new prob

---
+++ gatequest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+    int i=0,j,count=0;
+    for(j=-3;j<=3;j++){
+        if(j>=0 && i++){
+            printf("%d",i);
+            count+=j;
+        }
+    }
+     printf("%d",i);
+    count+=i;
+    printf("%d",count);
+}
